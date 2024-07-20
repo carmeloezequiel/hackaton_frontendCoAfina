7709fc580594fd9b06f05d02dc7d9941ba09585c
Gabriel Cardona <gabcardona0782@gmail.com>
2024-07-20 15:15:59 -0500

    Archivos y carpetas necesarias para el frontend del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import {LandingPageComponent} from './pages/landing-page/landing-page.component';
+import { DetectionPageComponent } from './pages/detection-page/detection-page.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+import path from 'path';
+
+
+export const routes: Routes = [
+  {path: '', component: LandingPageComponent},
+  {path: 'detection', component: DetectionPageComponent},
+  {path:'about', component: AboutComponent},
+  {path:'contact', component: ContactComponent}
+
+];
