4667ba17d20c4207373c6dd5000c2ca44b44acc2
Gabriel Cardona <gabcardona0782@gmail.com>
2024-07-20 18:48:09 -0500

    Se agrega la importación de CommonModule a detection-page component

--- src/app/pages/detection-page/detection-page.component.ts
+++ src/app/pages/detection-page/detection-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import * as ExifReader from 'exifreader';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-detection-page',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './detection-page.component.html',
   styleUrls: ['./detection-page.component.scss']
 })
